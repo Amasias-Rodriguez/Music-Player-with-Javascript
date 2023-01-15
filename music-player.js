398fba74864e507cbd594c4c4d87c72d9835c2dc
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-01-14 22:05:01 -0600

    Update music-player.js

--- music-player.js
+++ music-player.js
@@ -2,17 +2,17 @@ const songList = [
     {
         title: "Al Rojo Vivo - Charlie Cardona",
         file: "alrojovivo.mp3",
-        cover: "music.png"
+        cover: "cover.png"
     },
     {
         title: "Alcoba - Grupo Raices",
         file: "alcobaraices.mp3",
-        cover: "music.png"
+        cover: "cover.png"
     },
     {
         title: "Alejate de mi - Frankie Ruiz ",
         file: "alejatedemi.mp3",
-        cover: "music.png"
+        cover: "cover.png"
     }
 ]
 
@@ -20,23 +20,36 @@ let actualSong = null
 
 // Capturar elementos del dom 
 
-const songs = document.getElementById('songs')
-const audio = document.getElementById('audio')
-const cover = document.getElementById('cover')
-const title = document.getElementById('title')
-// Cargar canciones y mostrar listado
+const songs = document.getElementById("songs")
+const audio = document.getElementById("audio")
+const cover = document.getElementById("cover")
+const title = document.getElementById("title")
+const play = document.getElementById("play")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+
+play.addEventListener("click", () => {
+    if (audio.paused){
+        playSong()
+    } else {
+        pauseSong()
+    }
+})
+
+next.addEventListener("click", () => nextSong())
+prev.addEventListener("prev", () => prevSong())
 
 function loadSongs(){
     songList.forEach((song, index) => {
         console.log(index)
-        const li = document.createElement('li')
+        const li = document.createElement("li")
 
-        const link = document.createElement('a')
+        const link = document.createElement("a")
 
         link.textContent = song.title
-        link.href = '#'
+        link.href = "#"
 
-        link.addEventListener('click', () =>  loadSong(index))
+        link.addEventListener("click", () =>  loadSong(index))
 
         li.appendChild(link)
 
@@ -50,18 +63,36 @@ function loadSong(songIndex){
         changeActiveClass(actualSong, songIndex)
         actualSong = songIndex
         audio.src = "./audio/" + songList[songIndex].file
-        audio.play()
-        changeCover(songIndex)
-        
+        playSong()
+        changeCover(songIndex)       
     }
 }
 
-function changeActiveClass(lastIndex, newIndex){
-    const links = document.querySelectorAll('a')
-    if (lastIndex === null){
-        links[lastIndex].classList.remove('active')
+function updateControls(){
+    if (audio.paused){
+        play.classList.remove("fa-pause")
+        play.classList.add("fa-play")
+    } else {
+        play.classList.add("fa-pause")
+        play.classList.remove("fa-play")
     }
-    links[newIndex].classList.add('active')
+}
+
+function playSong(){
+   if (actualSong === null){
+    audio.play()
+    updateControls()
+   }
+}
+
+
+
+function changeActiveClass(lastIndex, newIndex){
+    const links = document.querySelectorAll("a")
+    if (lastIndex === null){
+        links[lastIndex].classList.remove("active")
+    }
+    links[newIndex].classList.add("active")
 }
 
 function changeCover(songIndex){
@@ -70,6 +101,17 @@ function changeCover(songIndex){
 
 function changeSongTitle(songIndex){
     title.innerText = songList[songIndex].title
+}
+
+function prevSong(){
+    actualSong--
+    playSong()
+    
+}
+
+function nextSong(){
+    actualSong--
+    playSong()
 }
 
 // Go!
